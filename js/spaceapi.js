364c077049eefa8f0985003d364433e1db74bb2c
Danilo Bargen <mail@dbrgn.ch>
2015-03-28 23:26:02 +0100

    Updated comments in JavaScript

--- js/spaceapi.js
+++ js/spaceapi.js
@@ -1,16 +1,21 @@
 jQuery(function ($) {
-    /* You can safely use $ in this code block to reference jQuery */
+    // Download the JSON data from the SpaceAPI endpoint
     $.getJSON(php_vars.url, function(data) {
+
+        // Show status
         if (data.state.open) {
             var status_value = 'Open';
         } else {
             var status_value = 'Closed';
         }
+
+        // If a message is provided, append it to the status
         if (data.state.message) {
             $('.opening_status_value').text(status_value + ':');
             $('.opening_status_message').text(data.state.message);
         } else {
             $('.opening_status_value').text(status_value);
         }
+
     });
 });
